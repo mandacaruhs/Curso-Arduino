34135ec8f7d7b964677c17c6c3cbf6a571feee71
Sérgio Pugliesi <sergiopugli@gmail.com>
2018-09-09 21:26:04 -0300

    Modificação em estrutura do src Arduino e Experimento 3
    Modificação de estrutura de source de arduino no experimento 1. O source arduino precisa estar em uma pasta homonima, mas para arduivos maiores um nome mais simples é necessário.
    Adição dos conteúdos do experimento 3

--- 1-Piscar Led/PiscarLed/PiscarLed.ino
+++ 1-Piscar Led/PiscarLed/PiscarLed.ino
@@ -1,11 +1,19 @@
 /*
-Examplo 01
+  Exemplo 1
+  Criado por: MarceloBiao
+  Data:       09/09/2018
 
-Pisca Led
+  Pisca alternadamente um LED.
+
+  Materiais:
+    1x LED
+    1x Resistor p/ LED
 */
+
+
 void setup()
 {
-    //Define o pino 13 como um pino de saida  
+    //Define o pino 13 como um pino de saida
     pinMode(13, OUTPUT);
 }
 
